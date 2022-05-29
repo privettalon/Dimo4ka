902d470fa5b8eb909140083eb6daed3fb6ca150c
solomiia.lisova <solomiia.lisova@avenga.com>
2022-05-29 14:50:24 +0200

    add BaseTest

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,2 +1,28 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
 public class BaseTest {
+    ChromeDriver driver = new ChromeDriver();
+
+    @BeforeTest
+    public void setup(){
+        WebDriverManager.chromedriver().setup();
+        driver.manage().window().maximize();
+
+        driver.get("http://www.google.com");
+
+    }
+
+    @Test
+    public void test1(){
+
+    }
+
+    @AfterMethod
+    public void closeDriver(){
+        driver.close();
+    }
 }
